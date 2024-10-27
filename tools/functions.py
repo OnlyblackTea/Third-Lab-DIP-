826d40041642559bcdb8a801ea8e150fee2a772c
OnlyblaackTea <804559276@qq.com>
2024-10-28 01:01:12 +0800

    [feat] 新增了几个数学函数

--- tools/functions.py
+++ tools/functions.py
@@ -4,7 +4,7 @@
   date          : 2024/10/27
   Description   : 基本函数
 """
-
+import math
 import numpy as np
 
 # 卷积
@@ -64,3 +64,34 @@ def apply_threshold(img, threshold):
     ret = np.zeros_like(img)
     ret[img > threshold] = img[img > threshold]
     return ret
+
+def clip_and_convert(func):
+    def wrapper(*args, **kwargs):
+        ret = func(*args, **kwargs)
+        return np.clip(ret, 0, 255).astype(np.uint8)
+    return wrapper
+
+# log变换
+@clip_and_convert
+def T_log(r, base=2):
+    norm_r = r/255 + 1
+    return math.log(r, base) * 255
+
+# 灰度反转
+@clip_and_convert
+def T_reverse(r):
+    return 255 - r
+
+# 线性变换
+@clip_and_convert
+def T_linear(r, a, b, c, d):
+    """
+    线性变换
+    :param r: 输入像素值
+    :param a: 暗处拐点输入值
+    :param b: 亮处拐点输入值
+    :param c: 暗处拐点输出值
+    :param d: 亮处拐点输出值
+    :return s: 变换后的像素值
+    """
+    return ((d - c) / (b - a)) * (r - a) + c
